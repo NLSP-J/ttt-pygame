ba897a5d192a8cad5f8b2ca70c7f5c67ba678603
NLSP-J <justin@nullspace.co>
2025-04-22 20:29:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ board = [['','',''],
 		 ['','',''],
 		 ['','','']]
 
-# x_img = pg.image.load("X_modified.png")
-# o_img = pg.image.load("o_modified.png")
+x_img = pg.image.load("./assets/images/X_modified.png")
+o_img = pg.image.load("./assets/images/o_modified.png")
 
-# x_img = pg.transform.scale(x_img, (80, 80))
-# o_img = pg.transform.scale(o_img, (80, 80))
+x_img = pg.transform.scale(x_img, (80, 80))
+o_img = pg.transform.scale(o_img, (80, 80))
 
 
 def update_text():
