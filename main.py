f965905572c0d13021c1bc0edb4a7d9646f5d2c7
NLSP-J <justin@nullspace.co>
2025-04-22 20:21:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,81 @@
+import pygame as pg
+import time
 import asyncio
-import pygame
 
-pygame.init()
-pygame.display.set_mode((320, 240))
-clock = pygame.time.Clock()
+pg.init()
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+win_width = 400
+win_height = 500
+screen = pg.display.set_mode([win_width, win_height])
+pg.display.set_caption('Tic-Tac-Toe')
+
+font = pg.font.Font(None, 30)
+
+player = 'x'
+moves = 0
+draw = False
+winner = False
+
+b_width = 400
+b_height = 400
+board = [['','',''],
+		 ['','',''],
+		 ['','','']]
+
+# x_img = pg.image.load("X_modified.png")
+# o_img = pg.image.load("o_modified.png")
+
+# x_img = pg.transform.scale(x_img, (80, 80))
+# o_img = pg.transform.scale(o_img, (80, 80))
+
+
+def update_text():
+
+	global draw, player
+
+	if draw:
+		text = "It's a draw!"
+	elif winner != False:
+		text = f"{winner} wins!"
+	else:
+		text = f"{player}'s turn"
+
+	screen.fill(black, (0, 400, 500, 100))
+	text = font.render(text, True, white)
+	text_rect = text.get_rect(center=(b_width / 2, win_height-50))
+	screen.blit(text, text_rect)	
+
+def game_window():
+
+	screen.fill(white)
+
+	# drawing vertical lines
+	pg.draw.line(screen, black, (b_width / 3, 0),
+				(b_width / 3, b_height), 7)
+	pg.draw.line(screen, black, (b_width / 3 * 2, 0),
+				(b_width / 3 * 2, b_height), 7)
+
+	# drawing horizontal lines
+	pg.draw.line(screen, black, (0, b_height / 3),
+				(b_width, b_height / 3), 7)
+	pg.draw.line(screen, black, (0, b_height / 3 * 2),
+				(b_width, b_height / 3 * 2), 7)
+	
+	update_text()
 
 async def main():
-    count = 60
+	game_window()
+	while True:
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				running = False
 
-    while True:
-        print(f"{count}: Hello from Pygame")
-        pygame.display.update()
-        await asyncio.sleep(0)  # You must include this statement in your main loop. Keep the argument at 0.
-
-        if not count:
-            pygame.quit()
-            return
-        
-        count -= 1
-        clock.tick(60)
-
+		pg.display.flip()
+		await asyncio.sleep(0)
+		
 asyncio.run(main())
+
